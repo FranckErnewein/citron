aa825b441013808c8275e8265678889303b5f288
Franck Ernewein <franck@new-host-2.home>
2011-04-27 01:37:38 +0200

    bundle update : "password" = > Password

--- antoine.sql
+++ antoine.sql
@@ -3,17 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Sam 16 Avril 2011 à 17:21
+-- Généré le : Mer 27 Avril 2011 à 01:36
 -- Version du serveur: 5.0.41
 -- Version de PHP: 5.2.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données: `antoine`
@@ -26,14 +20,14 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE `article_need` (
-  `id` int(32) NOT NULL auto_increment,
-  `demand_id` int(32) NOT NULL,
-  `name` varchar(128) collate utf8_unicode_ci NOT NULL,
-  `quantity` int(16) NOT NULL,
-  `price_need` float NOT NULL,
-  `freeze` tinyint(1) NOT NULL,
-  `fixed` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` int(32) NOT NULL auto_increment,
+	  `demand_id` int(32) NOT NULL,
+	  `name` varchar(128) collate utf8_unicode_ci NOT NULL,
+	  `quantity` int(16) NOT NULL,
+	  `price_need` float NOT NULL,
+	  `freeze` tinyint(1) NOT NULL,
+	  `fixed` tinyint(1) NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
@@ -51,14 +45,14 @@ INSERT INTO `article_need` VALUES(3, 1, 'Radis géant de colombie', 1, 18, 0, 0)
 --
 
 CREATE TABLE `article_offer` (
-  `id` int(32) NOT NULL auto_increment,
-  `offer_id` int(32) NOT NULL,
-  `artcile_need_id` int(32) NOT NULL,
-  `creation_date` datetime NOT NULL,
-  `last_update` datetime NOT NULL,
-  `price` float NOT NULL,
-  `new_name` varchar(128) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` int(32) NOT NULL auto_increment,
+	  `offer_id` int(32) NOT NULL,
+	  `artcile_need_id` int(32) NOT NULL,
+	  `creation_date` datetime NOT NULL,
+	  `last_update` datetime NOT NULL,
+	  `price` float NOT NULL,
+	  `new_name` varchar(128) collate utf8_unicode_ci NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
@@ -75,14 +69,14 @@ INSERT INTO `article_offer` VALUES(2, 1, 3, '2011-03-26 00:05:24', '2011-03-26 0
 --
 
 CREATE TABLE `contact` (
-  `id` int(32) NOT NULL auto_increment,
-  `company_id` int(32) NOT NULL,
-  `firstname` varchar(64) collate utf8_unicode_ci NOT NULL,
-  `lastname` varchar(64) collate utf8_unicode_ci NOT NULL,
-  `email` varchar(64) collate utf8_unicode_ci NOT NULL,
-  `cellphone` varchar(64) collate utf8_unicode_ci NOT NULL,
-  `phone` varchar(64) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` int(32) NOT NULL auto_increment,
+	  `company_id` int(32) NOT NULL,
+	  `firstname` varchar(64) collate utf8_unicode_ci NOT NULL,
+	  `lastname` varchar(64) collate utf8_unicode_ci NOT NULL,
+	  `email` varchar(64) collate utf8_unicode_ci NOT NULL,
+	  `cellphone` varchar(64) collate utf8_unicode_ci NOT NULL,
+	  `phone` varchar(64) collate utf8_unicode_ci NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
@@ -97,18 +91,18 @@ CREATE TABLE `contact` (
 --
 
 CREATE TABLE `demand` (
-  `id` int(32) NOT NULL auto_increment,
-  `user_id` int(32) NOT NULL,
-  `reference` varchar(64) collate utf8_unicode_ci NOT NULL,
-  `creation_data` datetime NOT NULL,
-  `last_update` datetime NOT NULL,
-  `end_date` datetime NOT NULL,
-  `title` varchar(128) collate utf8_unicode_ci NOT NULL,
-  `city` varchar(128) collate utf8_unicode_ci NOT NULL,
-  `city_code` varchar(32) collate utf8_unicode_ci NOT NULL,
-  `x` double NOT NULL,
-  `y` double NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` int(32) NOT NULL auto_increment,
+	  `user_id` int(32) NOT NULL,
+	  `reference` varchar(64) collate utf8_unicode_ci NOT NULL,
+	  `creation_data` datetime NOT NULL,
+	  `last_update` datetime NOT NULL,
+	  `end_date` datetime NOT NULL,
+	  `title` varchar(128) collate utf8_unicode_ci NOT NULL,
+	  `city` varchar(128) collate utf8_unicode_ci NOT NULL,
+	  `city_code` varchar(32) collate utf8_unicode_ci NOT NULL,
+	  `x` double NOT NULL,
+	  `y` double NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
@@ -124,12 +118,12 @@ INSERT INTO `demand` VALUES(1, 2, 'REF345', '2011-03-20 21:17:20', '2011-03-20 2
 --
 
 CREATE TABLE `dictionary` (
-  `id` int(11) NOT NULL auto_increment,
-  `lang_key` varchar(8) collate utf8_unicode_ci NOT NULL,
-  `namespace` varchar(32) collate utf8_unicode_ci NOT NULL,
-  `key` varchar(40) collate utf8_unicode_ci NOT NULL,
-  `value` text collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` int(11) NOT NULL auto_increment,
+	  `lang_key` varchar(8) collate utf8_unicode_ci NOT NULL,
+	  `namespace` varchar(32) collate utf8_unicode_ci NOT NULL,
+	  `key` varchar(40) collate utf8_unicode_ci NOT NULL,
+	  `value` text collate utf8_unicode_ci NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
 
 --
@@ -142,7 +136,7 @@ INSERT INTO `dictionary` VALUES(3, 'fr', 'login', 'Wrong', 'Mauvais email / mot 
 INSERT INTO `dictionary` VALUES(4, 'fr', 'login', 'BtnConnect', 'Connexion');
 INSERT INTO `dictionary` VALUES(5, 'fr', 'user', 'Logout', 'Deconnexion');
 INSERT INTO `dictionary` VALUES(6, 'en', 'login', 'Login', 'Email');
-INSERT INTO `dictionary` VALUES(7, 'en', 'login', 'Password', 'password');
+INSERT INTO `dictionary` VALUES(7, 'en', 'login', 'Password', 'Password');
 INSERT INTO `dictionary` VALUES(8, 'en', 'login', 'Wrong', 'Bad email / password');
 INSERT INTO `dictionary` VALUES(9, 'en', 'login', 'BtnConnect', 'Connect');
 INSERT INTO `dictionary` VALUES(10, 'en', 'user', 'Logout', 'Logout');
@@ -156,10 +150,10 @@ INSERT INTO `dictionary` VALUES(12, 'en', 'user', 'LogAs', 'Login as ');
 --
 
 CREATE TABLE `lang` (
-  `id` tinyint(4) NOT NULL auto_increment,
-  `key` varchar(8) collate utf8_unicode_ci NOT NULL,
-  `label` varchar(32) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` tinyint(4) NOT NULL auto_increment,
+	  `key` varchar(8) collate utf8_unicode_ci NOT NULL,
+	  `label` varchar(32) collate utf8_unicode_ci NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
@@ -174,12 +168,12 @@ CREATE TABLE `lang` (
 --
 
 CREATE TABLE `offer` (
-  `id` int(32) NOT NULL auto_increment,
-  `demand_id` int(32) NOT NULL,
-  `user_id` int(32) NOT NULL,
-  `creation_date` datetime NOT NULL,
-  `last_update` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` int(32) NOT NULL auto_increment,
+	  `demand_id` int(32) NOT NULL,
+	  `user_id` int(32) NOT NULL,
+	  `creation_date` datetime NOT NULL,
+	  `last_update` datetime NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
@@ -195,11 +189,11 @@ INSERT INTO `offer` VALUES(1, 1, 3, '2011-03-26 00:03:19', '2011-03-26 00:03:23'
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `email` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `password` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `type` int(8) NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` int(11) NOT NULL auto_increment,
+	  `email` varchar(50) collate utf8_unicode_ci NOT NULL,
+	  `password` varchar(50) collate utf8_unicode_ci NOT NULL,
+	  `type` int(8) NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
@@ -217,9 +211,9 @@ INSERT INTO `users` VALUES(3, 'bengalo@yahoo.fr', '123456', 3);
 --
 
 CREATE TABLE `user_type` (
-  `id` int(11) NOT NULL,
-  `desc` varchar(32) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
+	  `id` int(11) NOT NULL,
+	  `desc` varchar(32) collate utf8_unicode_ci NOT NULL,
+	  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -229,3 +223,4 @@ CREATE TABLE `user_type` (
 INSERT INTO `user_type` VALUES(1, 'admin');
 INSERT INTO `user_type` VALUES(2, 'announceur');
 INSERT INTO `user_type` VALUES(3, 'supplier');
+
