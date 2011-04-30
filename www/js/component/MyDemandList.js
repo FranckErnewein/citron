91181551f5a798dabd25fbafeb912dc7985df5c5
Franck Ernewein <franck@macbook-de-franck-ernewein-6.local>
2011-04-30 12:56:44 +0200

    skin list

--- www/js/component/MyDemandList.js
+++ www/js/component/MyDemandList.js
@@ -5,25 +5,19 @@
 		
 	}
 
-
 	utils.inherits(MyDemandList, pack.AbstractComponent);
-
 
 	MyDemandList.prototype.onDomReady = function(){
 		
 		var self = this;
-		//FIXME : do children selectore on AbstractComponent
-		$('a', this.node).click(function(){
+		//FIXME : do children selector on AbstractComponent
+		$('a.refresher', this.node).click(function(){
 			$(this).apiLink().done(function(data){
 				self.children[0].render(data);
 			});
-		});
+		}).click();
+
 	}
-
-	
-
-	
-
 
 	pack.MyDemandList = MyDemandList;
 
